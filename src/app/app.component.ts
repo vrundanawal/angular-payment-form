53fc6e64dd5ce0a56a25206956d09e46e9c251e1
vrundanawal <nawal.vrunda@gmail.com>
2022-08-09 11:28:10 +0100

    basic angular setup with adding Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'payment-form-pipes';
+  //name: string  = ''
+  name: string | undefined;
+  //method to handle name
+  handleOnName(value: string) {
+    this.name = value;
+  }
 }
